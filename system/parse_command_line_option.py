032aa9e59332ed054d02656151c059b34892a47c
BruceChen <bofengqiye7@gmail.com>
2015-01-05 23:18:59 +0800

    add system

---
+++ system/parse_command_line_option.py
@@ -0,0 +1,20 @@
+import sys 
+
+def getopts(argv):
+    opts = {} 
+    while argv:
+        if argv[0][0] == '-':
+            opts[argv[0]] = argv[1]
+            argv = argv[2:]
+        else:
+            argv = argv[1:]
+    return opts
+
+if __name__ == "__main__":
+    from sys import argv
+    myargs = getopts(argv)
+    
+    if '-i' in myargs:
+        print(myargs['-i'])
+    print(myargs)
+
