ed506a2cdc6655ebbb899151b639ff7623acccc5
BruceChen <bofengqiye7@gmail.com>
2015-01-20 21:29:16 +0800

    change something

--- CPLUSPLUS/Utilities/src/logging.cc
+++ CPLUSPLUS/Utilities/src/logging.cc
@@ -1,5 +1,6 @@
 #include "logging.h"
-
+#include <sstream>
+#include <stdio.h>
 using namespace Toyscode;
 using namespace Utilities;
 
@@ -11,40 +12,60 @@ namespace Toyscode
 		{ 
 			public:
 				using Loglevel = Logging::Loglevel; 
-				LoggingImp(Loglevel level,int old_errno,int line); 
+				LoggingImp(Loglevel level,int old_errno,const char *file ,int line); 
 				//members
-				Loglevel severity_;
+				Loglevel level_;
 				int line_num_; 
-				// std::ostream stream_;
+				std::ostringstream stream_;
 				int old_errno_;
-				// const char* file_;
+				const char* file_name_;
+				static  const char* log_level_name[];
 
 		};
 
-        //
-		Logging::LoggingImp::LoggingImp(Loglevel level,int old_errno,int line):severity_(level),line_num_(line),old_errno_(old_errno)
+		const char* Logging::LoggingImp::log_level_name[] = 
 		{
+			"TRACE",
+			"DEBUG",
+			"MSG",
+			"WARN",
+			"ERR",
+			"FATA", 
+		};
+        
+		//
+		Logging::LoggingImp::LoggingImp(Loglevel level,int old_errno,const char* file ,int line_num):level_(level),line_num_(line_num),stream_(),old_errno_(old_errno)
+		{
+			char message_head[512];
+			snprintf(message_head,sizeof(message_head),"%s %5d %s","time","thread id",log_level_name[level_]);
+			stream_ << message_head;
 		}
 
 
 
 		//Logging implementation
-		Logging::Logging(const char *file, int line_num):impl_(new LoggingImp(Loglevel::LOG_TRACE,__LINE__,0)) {}
+		Logging::Logging(const char *file, int line_num):impl_(new LoggingImp(Loglevel::LOG_TRACE,0,file,0)) 
+		{
+		}
 		
-		void Logging::set_log_level(Logging::Loglevel severity)
+		void Logging::set_log_level(Logging::Loglevel level)
 		{
 
-				impl_->severity_ = severity;
+				impl_->level_ = level;
 
 		}
 
 		Logging::Loglevel Logging::get_log_level()
 		{
-			return impl_->severity_;
+			return impl_->level_;
 		}
 		
 		
-		Logging::~Logging() = default;
+		Logging::~Logging()
+		{
+		
+		
+		}
 		
 		
 
