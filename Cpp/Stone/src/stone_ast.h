c816ddd715138fc88ab59c4b7c92653e4301fb03
BruceChen <bofengqiye7@gmail.com>
2015-03-10 17:14:26 +0800

    using Visitor Pattern to complete ast tree but there are may be some wrong design in ast_stone.h

--- Cpp/Stone/src/stone_ast.h
+++ Cpp/Stone/src/stone_ast.h
@@ -5,13 +5,33 @@ namespace Stone
 {
 	namespace Ast
 	{
+		//forward declaration
 		class AstNode;
 		class AstStatement;
 		class AstExpr;
+		class AstPrimary;
+		class AstFactor;
+		class AstNumber;
+		class AstIdentifier;
+		class AstString;
+		class AstOperation;
+		class AstBlock;
+		class AstSimple;
+		class AstProgram;
+
 
 		class AstVisitor;
 		class AstStatementVistor;
 		class AstExprVistor;
+		class AstFactorVisitor;
+		class AstPrimaryVisitor;
+		class AstNumberVistor;
+		class AstIdentifierVisitor;
+		class AstStringVisitor;
+		class AstOperationVistor;
+		class AstBlockVisitor;
+		class AstSimpleVisitor;
+		class AstProgramVisitor;
 
 
 		class AstNode : public std::enable_shared_from_this<AstNode>
@@ -34,6 +54,7 @@ namespace Stone
 
 				void accept(AstVisitor* visitor) = override;
 				virtual  void accept(AstStatementVistor * visitor) = 0; 
+			
 		};
 
 		class AstExpr:public AstNode 
@@ -44,6 +65,84 @@ namespace Stone
 				void accept(AstVisitor *visitor) override;
 				void accept(AstExprVistor *visitor) = 0;
 		};
+
+		class AstPrimary:public AstNode
+		{
+			public:
+				using Ptr = std::shared_ptr<AStPrimar>;
+				using WeakPtr = std::weak_ptr<AstPrimar>;
+				void accept(AstVisitor *visitor) override ;
+				void accept(AstPrimaryVisitor* visitor) = 0;
+		};
+		
+		class AstFactor:public AstNode 
+		{
+			public:
+				using Ptr = std::shared_ptr<AstFactor>;
+				using WeakPtr = std::shared_ptr<AstFactor>;
+				void accept(AstVisitor * vistor) = override;
+				void accept(AstNumberVisitor* visitor) = 0;
+		};
+
+		class AstNumber:public AstNode
+		{
+			public:
+				using Ptr = std::shared_ptr<AstNumber>;
+				using WeakPtr = std::weak_ptr<AstNumber>;
+				void accept(AstVisitor *visitor) = override;
+				void accept(AstNumberVistor *visitor) = 0;
+		
+		};
+
+		class AstIdentifier:public AstNode
+		{
+			public:
+				using Ptr= std::shared_ptr<AstIdentifier>;
+				using WeakPtr = std::weak_ptr<AstIdentifier>;
+				void accept(AstVistor *visitor) = override;
+				void accept(AstIdentifierVisitor* visitor) = 0;
+		
+		};
+		class AstString:public AstNode 
+		{
+			public:
+				using Ptr = std::shared_ptr<AstString>;
+				using WeakPtr = std::shared_ptr<AstString>;
+				void accept(AstVisitor * vistor) = override;
+				void accept(AstStringVisitor* visitor) = 0;
+		
+		};
+
+		class AstBlock:public AstNode
+		{ 
+			public:
+				using Ptr = std::shared_ptr<AstBlock>;
+				using WeakPtr = std::weak_ptr<AstBlock>;
+				void accept(AstVistor* visitor) = override;
+				void accept(AstNumberVisitor* visitor) = 0;
+		};
+
+		class AstSimple:public AstNode
+		{
+			public:
+				using Ptr = std::shared_ptr<AstSimple>;
+				using WeakPtr = std::weak_ptr<AstSimple>;
+				void accept(AstVisitor* visitor) = override;
+				void accept(AstSimpleVisitor* visitor) = 0;
+
+		}
+
+		class AstProgram:public AstNode
+		{
+			public:
+				using Ptr = std::shared_ptr<AstProgram>;
+				using WeakPtr = std::weak_ptr<AstProgram>;
+				void accept(AstVisitor* visitor) = override;
+				void accept(AstProgramVisitor* visitor) = 0 ;
+		}
+
+
+
 
 	}
 
