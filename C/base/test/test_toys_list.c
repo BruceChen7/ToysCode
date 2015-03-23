7c594c5251f8a8a778ded3a1f80f19d98961cbf8
BruceChen <bofengqiye7@gmail.com>
2015-03-23 21:21:37 +0800

    use Criterion to undertake unit test

--- C/base/test/test_toys_list.c
+++ C/base/test/test_toys_list.c
@@ -1,21 +1,20 @@
-#include "../src/toys_list.h"
-#include <check.h>		
+#include "../toys_list.h"
+#include <criterion/criterion.h>
 #include <string.h> 
 #include <stdio.h>
 
-START_TEST(test_toys_list_create)
+Test(test_toys_list_create,create)
 {
 
 	toys_list *list = toys_list_create();
-	ck_assert_ptr_eq(list->head,NULL);
-	ck_assert_ptr_eq(list->tail,NULL);
-	ck_assert_ptr_eq(list->free,NULL);
-	ck_assert_ptr_eq(list->dup,NULL);
-	ck_assert_ptr_eq(list->len, 0); 
+	assert_eq(list->head,NULL);
+	assert_eq(list->tail,NULL);
+	assert_eq(list->free,NULL);
+	assert_eq(list->dup,NULL);
+	assert_eq(list->len, 0); 
 }
-END_TEST 
 
-START_TEST(test_toys_list_add_node_head)
+Test(test_toys_list_add_node_head,add_node_head)
 {
 	//7->5->4->8
 	int value1 = 4;
@@ -32,57 +31,34 @@ START_TEST(test_toys_list_add_node_head)
 	toys_list_add_node_tail(list,&value4);
 
 
-	ck_assert_int_eq(list->len,4);
-	ck_assert_int_eq(*(int *)(list->head->value),7);
-	ck_assert_int_eq(*(int *)(list->head->next->value),5);
-	ck_assert_int_eq(*(int *)(list->head->next->next->value),4);
-	ck_assert_int_eq(*(int *)(list->tail->value),8);
+	assert_eq(list->len,4);
+	assert_eq(*(int *)(list->head->value),7);
+	assert_eq(*(int *)(list->head->next->value),5);
+	assert_eq(*(int *)(list->head->next->next->value),4);
+    assert_eq(*(int *)(list->tail->value),8);
 
 	toys_list_node *node = toys_list_index(list,0); 
-	ck_assert_int_eq(*(int *)(node->value),7); 
+	assert_eq(*(int *)(node->value),7); 
 
 	node = toys_list_index(list,1);
-	ck_assert_int_eq(*(int *)(node->value),5); 
+	assert_eq(*(int *)(node->value),5); 
 	
 	node = toys_list_index(list,2);
-	ck_assert_int_eq(*(int *)(node->value),4); 
+	assert_eq(*(int *)(node->value),4); 
 
 	node = toys_list_index(list, -1);
-	ck_assert_int_eq(*(int *)(node->value),8); 
+	assert_eq(*(int *)(node->value),8); 
 
 	node = toys_list_index(list, -2);
-	ck_assert_int_eq(*(int *)(node->value),4); 
+	assert_eq(*(int *)(node->value),4); 
 
 	node = toys_list_index(list,-3);
-	ck_assert_int_eq(*(int *)(node->value),5); 
+	assert_eq(*(int *)(node->value),5); 
 
 	node = toys_list_search_key(list,&value2);
-	ck_assert_int_eq(*(int *)node->value,5); 
+	assert_eq(*(int *)node->value,5); 
 
 
 }
-END_TEST
 
-Suite* sds_suit(void) 
-{
 
-	Suite *s = suite_create("Toy List Basic Api Testing ");
-	TCase* tc_core = tcase_create("core case");
-	tcase_add_test(tc_core,test_toys_list_create); 
-	tcase_add_test(tc_core,test_toys_list_add_node_head);
-	suite_add_tcase(s,tc_core);
-	return s; 
-}
-
-int main() 
-{
-	int number_failed ;
-	// the new suit to be test
-	Suite* s =  sds_suit(); 
-	SRunner*  sr = srunner_create(s);
-
-	srunner_run_all(sr,CK_NORMAL);
-	number_failed = srunner_ntests_failed(sr);
-	srunner_free(sr);
-	return 0; 
-}
--- C/base/test/test_toys_sds.c
+++ C/base/test/test_toys_sds.c
@@ -1,105 +1,80 @@
-#include "../src/toys_sds.h"
-#include <check.h>		
+#include "../toys_sds.h"
 #include <string.h> 
 #include <stdio.h>
+#include <criterion/criterion.h>
 
-START_TEST(test_toys_sds)
+Test(TestToysSds,Basic)
 {
 	sds test_string= "Hello world";
 
 	//create the first 2 word string from the test_string 
 	sds y =  toys_sds_new_len(test_string,2); 
-	ck_assert_str_eq("He",y);
+	assert_strings_eq("He",y);
 
 	//test for toys_sds_len
 	size_t len = toys_sds_len(y);
-	ck_assert_uint_eq(len,2);
+	assert_eq(len,2);
 
 	//test for toys_sds_new
 	sds x = toys_sds_new(test_string);
-	ck_assert_str_eq(test_string,x);
-	ck_assert_uint_eq(strlen(test_string),toys_sds_len(x));
+	assert_strings_eq(test_string,x);
+	assert_eq(strlen(test_string),toys_sds_len(x));
 
 	//test for toys_sds_empty
-	ck_assert_str_eq("",toys_sds_empty()); 
+	assert_strings_eq("",toys_sds_empty()); 
 
 	//test for toys_sds_toupper 
 	toys_sds_toupper(x);
-	ck_assert_str_eq(x,"HELLO WORLD");
+	assert_strings_eq(x,"HELLO WORLD");
 
 	//test for toys_sds_tolower
 	toys_sds_tolower(x);
-	ck_assert_str_eq(x,"hello world"); 
+	assert_strings_eq(x,"hello world"); 
 
 	//test for toys_sds_avail
-	ck_assert_int_eq(toys_sds_avail(x),0);
+	assert_eq(toys_sds_avail(x),0);
 
 	//test for toys_sds_expand_size
 	//the size of "hello world" is 11
 	//then we expand the size of the new_string2 to (11+8)*2
 	//now the available free space is (11+8)*2 -11,which is 27
-	ck_assert_int_eq(toys_sds_len(x),11);
+	assert_eq(toys_sds_len(x),11);
 	x = toys_sds_expand_size(x,8);
-	ck_assert_int_eq(toys_sds_avail(x),27);
+	assert_eq(toys_sds_avail(x),27);
 
 	//test for toys_sds_cat
 	y = toys_sds_new(" Bruce Chen");
 	x = toys_sds_cat_len(x,y,toys_sds_len(y));
-	ck_assert_str_eq(x,"hello world Bruce Chen"); 
+	assert_strings_eq(x,"hello world Bruce Chen"); 
 
 	//to ensure y has enough space 
 	y = toys_sds_expand_size(y,14);
-	ck_assert_str_eq(y," Bruce Chen");
+	assert_strings_eq(y," Bruce Chen");
 
 	//when there is enough space 
 	//x is copied into y
 	toys_sds_cpy(y,x);
-	ck_assert_str_eq(y,x);
+	assert_strings_eq(y,x);
 	
 	//when there is not enough space
 	//the resturned sds is the copied x
 	y  = toys_sds_empty();
 	y = toys_sds_cpy(y,x);
-	ck_assert_str_eq(y,x); 
+	assert_strings_eq(y,x); 
 
 }
-END_TEST 
 
-START_TEST(advanced_test_sds) 
+Test(ADVANCED_SDS ,ADVANCED) 
 {
 	sds x = toys_sds_new("foo");
 	sds y = toys_sds_cat_snprintf(x,"%d %d %d",1,2,3);
-	ck_assert_str_eq(y,"foo1 2 3");
-	ck_assert_str_eq(x,"foo1 2 3");
+	assert_strings_eq(y,"foo1 2 3");
+	assert_strings_eq(x,"foo1 2 3");
 	
 	x = toys_sds_empty();
 	y = toys_sds_cat_snprintf(x,"%s %s %s","foo","bar","chen"); 
-	ck_assert_str_eq(x,"foo bar chen");
-	ck_assert_str_eq(y,"foo bar chen");
-
+	assert_strings_eq(x,"foo bar chen");
+	assert_strings_eq(y,"foo bar chen"); 
 
 }
-END_TEST
 
-Suite* sds_suit(void) {
-
-	Suite *s = suite_create("Toy SDS Basic Api Testing ");
-	TCase* tc_core = tcase_create("core case");
-	tcase_add_test(tc_core,test_toys_sds); 
-	tcase_add_test(tc_core,advanced_test_sds);
-	suite_add_tcase(s,tc_core);
-	return s; 
-}
-
-int main() 
-{
-	int number_failed ;
-	// the new suit to be test
-	Suite* s =  sds_suit(); 
-	SRunner*  sr = srunner_create(s);
-
-	srunner_run_all(sr,CK_NORMAL);
-	number_failed = srunner_ntests_failed(sr);
-	srunner_free(sr);
-	return 0; 
-}
