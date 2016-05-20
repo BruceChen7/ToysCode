edab09842c37bba2f042c6d100b4ca540328cbd9
BruceChen <bofengqiye7@gmail.com>
2016-05-20 22:42:19 +0800

    Fix bugs to calculate.c

--- C/Lispy/calculate.c
+++ C/Lispy/calculate.c
@@ -17,6 +17,7 @@ enum {
     LVAL_SEPR,
 };
 
+// Represent a value regardless of error value or right value
 typedef struct lval{
     // What the value is
     int type;
@@ -80,11 +81,6 @@ void lval_del(lval* v) {
     free(v);
 }
 
- lval* lval_read_num(mpc_ast_t* t) {
-     errno = 0;
-     long x = strtol(t->contents, NULL, 10);
-     return errno != ERANGE ? lval_num(x): lval_err("invalid number");
- }
 
 lval* lval_add(lval* v, lval* x) {
     v->count++;
@@ -92,6 +88,7 @@ lval* lval_add(lval* v, lval* x) {
     v->cell[v->count-1] = x;
     return v;
 }
+
 
 // Delete a value
 lval* lval_pop(lval* v, int i) {
@@ -107,6 +104,14 @@ lval* lval_take(lval* v, int i) {
     lval_del(v);
     return x;
 }
+
+
+lval* lval_read_num(mpc_ast_t* t) {
+    errno = 0;
+    long x = strtol(t->contents, NULL, 10);
+    return errno != ERANGE ? lval_num(x): lval_err("invalid number");
+}
+
 
 lval* lval_read(mpc_ast_t* t) {
 
@@ -134,7 +139,7 @@ lval* lval_read(mpc_ast_t* t) {
             continue;
         }
 
-        if(strcmp(t->children[i]->contents, ")") ==0 ) {
+        if(strcmp(t->children[i]->contents, ")") == 0 ) {
             continue;
         }
 
@@ -145,7 +150,7 @@ lval* lval_read(mpc_ast_t* t) {
         if(strcmp(t->children[i]->contents, "}") == 0 ) {
             continue;
         }
-        if(strcmp(t->children[i]->contents, "regex") == 0 ) {
+        if(strcmp(t->children[i]->tag, "regex") == 0 ) {
             continue;
         }
         x = lval_add(x, lval_read(t->children[i]));
@@ -208,9 +213,75 @@ lval* buildin_op(lval* a, char* op) {
 
     while(a->count > 0) {
         lval* y = lval_pop(a, 0);
+
+        if(strcmp(op, "+") == 0) {
+            x->num += y->num;
+        }
+
+        if(strcmp(op, "-") == 0) {
+            x->num -= y->num;
+        }
+
+        if(strcmp(op, "*") == 0) {
+            x->num *= y->num;
+        }
+
+        if(strcmp(op, "/") == 0) {
+            if(y->num == 0) {
+                lval_del(x);
+                lval_del(y);
+                x = lval_err("Division by zero.");
+                break;
+            }
+            x->num /= y->num;
+        }
+        lval_del(y);
     }
 
-    lval_del(x);
+    lval_del(a);
+    return x;
+}
+
+lval* lval_eval(lval* v);
+
+lval* lval_eval_sexpr(lval* v) {
+    for(int i = 0 ; i < v->count ; i++) {
+        v->cell[i] = lval_eval(v->cell[i]);
+    }
+
+    for(int i = 0 ; i < v->count; i++) {
+        if(v->cell[i]->type  == LVAL_ERR) {
+            return lval_take(v, i);
+        }
+    }
+
+    if(v->count == 0) {
+        return lval_take(v, 0);
+    }
+
+    if(v->count == 1) {
+        return lval_take(v, 0);
+    }
+
+    lval* f = lval_pop(v, 0);
+
+    if(f->type != LVAL_SYM) {
+        lval_del(f);
+        lval_del(v);
+        return lval_err("S-expression does not with symbol");
+    }
+
+    lval* result = buildin_op(v, f->sym);
+    lval_del(f);
+    return  result;
+}
+
+
+lval* lval_eval(lval* v) {
+    if(v->type == LVAL_SEPR) {
+        return lval_eval_sexpr(v);
+    }
+    return v;
 }
 
 int main() {
@@ -237,8 +308,7 @@ int main() {
         char* input = readline("lispy> ");
         mpc_result_t r;
         if(mpc_parse("<stdin>", input, Lispy, &r)) {
-//            mpc_ast_print(r.output);
-            lval* x = lval_read(r.output);
+            lval* x = lval_eval(lval_read(r.output));
             lval_println(x);
             lval_del(x);
             mpc_ast_delete(r.output);
