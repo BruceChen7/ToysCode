780f3d221d4268f194e27dfa526435bdf8ef51ed
BruceChen7 <bofengqiye7@gmail.com>
2016-12-23 11:43:15 +0800

    Add echo_client.py

---
+++ Python/Networking/EasyScript/echo_client.py
@@ -0,0 +1,18 @@
+#!/user/bin/python
+#-*- coding:utf-8 -*-
+
+"""
+It's a script about echo client
+"""
+import socket
+SOCK = socket.socket()
+
+HOST = socket.gethostname()
+PORT = 8000
+SOCK.connect((HOST, PORT))
+
+while True:
+    SEND_DATA = raw_input()
+    SOCK.send(SEND_DATA)
+    print SOCK.recv(1024)
+
