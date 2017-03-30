7a9640f506f8cc05a6e74ff27e7bd566258e7cb5
BruceChen7 <bofengqiye7@gmail.com>
2017-03-30 10:25:36 +0800

    Add more to tinyHTTP main.py

--- Python/tinyHTTP/main.py
+++ Python/tinyHTTP/main.py
@@ -1,8 +1,9 @@
 import socket 
+import os
 
+_server_string = "Server: toyhttp/0.1\r\n"
 def unimplement(conn):
     buf = "HTTP/1.0 501 Method Not Implement\r\n"
-    sever_string = "Server: toyhttp/0.1\r\n"
     html = """
     <html>
         <head>
@@ -18,11 +19,42 @@ def unimplement(conn):
     </html>
     """
     try:
-        conn.send(buf + sever_string + html) 
+        conn.send(buf + _server_string + html) 
     except Exception, e:
         print e
 
+
+def notFound(conn):
+    buf = "HTTP/1.0 404 NOT FOUND \r\n"
+    conn.send(buf)
+    conn.send(_server_string)
+    buf = "Content-Type: text/html\r\n"
+    conn.send(buf)
+    buf = "\r\n"
+    conn.send(buf)
+    buf = "<html><title>Not Found</title>\r\n"
+    conn.send(buf)
+    buf = "<body><p>the server could not fulfill your ruquest because the request you specified"
+    conn.send(buf)
+    buf = "is unavailable or nonexistent </p> \r\n"
+    conn.send(buf)
+    buf = "</body></html>\r\n"
+    conn.send(buf)
+
+
+def handleGetRequest(uri, conn): 
+    uri = uri.strip()
+    absolute_path = "htdocs" + uri 
+    if uri == '/':
+        absolute_path = "htdocs/index.html"
+    try:
+        file_stat = os.stat(absolute_path)
+    except OSError:
+        notFound(conn)
+
+
 def handleHTTPRequest(conn, addr):
+    print "Accept request from ", addr
     lines = conn.recv(4096)
     line_list = lines.splitlines()
     if len(line_list) == 0:
@@ -36,9 +68,13 @@ def handleHTTPRequest(conn, addr):
     else:
         method = http_first_header[0].upper()
         if method == "GET" or method == "POST":
-            print "method GET"
+            url = http_first_header[1]
+            uri, question, param = url.partition('?')
+            if question is '':
+                handleGetRequest(uri, conn)
+            else:
+                pass 
         else:
-            unimplement(conn)
             unimplement(conn)
     conn.close()
 
